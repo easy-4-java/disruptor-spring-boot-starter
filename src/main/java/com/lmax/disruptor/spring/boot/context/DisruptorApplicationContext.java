39765a2e5d2223a76a574eaf0a03f09b4f11655e
wandl-6A72h <hiwepy@gmail.com>
2026-07-07 21:38:45 +0800

    refactor: 移除已迁移到 disruptor-extension (2.0.x) 的 inlined 类

--- src/main/java/com/lmax/disruptor/spring/boot/context/DisruptorApplicationContext.java
+++ src/main/java/com/lmax/disruptor/spring/boot/context/DisruptorApplicationContext.java
@@ -16,9 +16,9 @@
 package com.lmax.disruptor.spring.boot.context;
 
 
-import com.lmax.disruptor.spring.boot.context.event.DisruptorEventPublisher;
+import com.lmax.disruptor.event.DisruptorEventPublisher;
 import com.lmax.disruptor.spring.boot.event.DisruptorApplicationEvent;
-import com.lmax.disruptor.spring.boot.event.DisruptorEvent;
+import com.lmax.disruptor.event.DisruptorEvent;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
